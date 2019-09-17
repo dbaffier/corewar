c68bfe3ed2fc62db912faf713b4915dab57d38ba
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-09-17 22:07:22 +0200

    [COREWAR] update main

--- incs/vm.h
+++ incs/vm.h
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 18:03:25 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/09/17 19:16:39 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/09/17 22:06:42 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,18 @@
 
 typedef struct		s_process
 {
-	char		position;
-	char		*name;
-	char		reg[REG_NUMBER][REG_SIZE];
-	char		pc[REG_SIZE];
-	char		carry;
+	char			position;
+	char			*name;
+	char			reg[REG_NUMBER][REG_SIZE];
+	char			pc[REG_SIZE];
+	char			carry;
 }					t_process;
 
 typedef struct		s_env
 {
-	size_t		nb_cycle;
-	t_process	proc[MAX_PLAYERS];
+	char			*progname;
+	size_t			nb_cycle;
+	t_process		proc[MAX_PLAYERS];
 }					t_env;
 
 #endif
--- vm/main.c
+++ vm/main.c
@@ -1,4 +1,4 @@
- /* ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 18:05:10 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/09/17 18:05:49 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/09/17 22:06:54 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 static int		usage(char *progname)
 {
 	ft_dprintf(2, "Usage :\n%s", progname);
-	ft_dprintf(2, "[-dump nbr_cycles] [[-n number] champion1.cor] ...]\n");
+	ft_dprintf(2, "[-dump nbr_cycles] [[-n number] champion1.cor] ...]\n ");
 	return (1);
 }
 
 void			get_args(char **av, t_env *e)
 {
-	int				i;
-	char			*tmp;
-	int				n;
+	int		i;
+	char	*tmp;
+	int		n;
 
 	i = 0;
 	while (av[i])
@@ -36,17 +36,18 @@ void			get_args(char **av, t_env *e)
 		else if (ft_strequ(av[i], "-n"))
 		{
 			n = ft_atoi(av[i + 1]);
-			if (ft_isdigit(n) && n > 0 && n < MAX_PLAYERS)
-				e->n = n;
+			if (!ft_isdigit(n) || n <= 0 || n > MAX_PLAYERS)
+				return (ft_dprintf(2, "%s: wrong parameter `%s'\n", e->progname, av[i]));
+			e->nb_cycle = n;
 		}
 		else if ((tmp = ft_strrchr(av[i], '.')) && ft_strequ(tmp, ".cor"))
 		{
-
+			
 		}
 		else if (av[i][0] == '-')
-			return (ft_dprintf(2, "%s: wrong parameter.\n", av[i]));
+			return (ft_dprintf(2, "%s: wrong parameter `%s'\n", e->progname, av[i]));
 		else
-			return (ft_dprintf(2, "%s: wrong champion filename.\n", av[i]));
+			return (ft_dprintf(2, "%s: invalid filename `%s'\n", e->progname, av[i]));
 		i++;
 	}
 }
@@ -56,8 +57,9 @@ int				main(int ac, char **av)
 	t_env	e;
 
 	ft_memset(&env, 0, sizeof(e));
-	if (ac < 3)
+	if (!(e.progname = ft_strrchr(av[0])))
+		e.progname = av[0];
+	if (ac < 3 || get_args(av, &e))
 		return (usage(av[0]));
-	get_args(av, &e);
 	return (42);
 }
