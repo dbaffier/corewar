c833b0605323ae366906058eb967c5b964a9922d
Meiline celine MONIER <mmonier@e2r3p20.42.fr>
2019-10-23 00:40:23 +0200

    merge

--- asm_dir/test_dir/crack.s
+++ asm_dir/test_dir/crack.s
@@ -1,11 +1,10 @@
 .name "i"
 .comment "blabla"
 
-label01: 
-ldi r1,r1,r1#nullr1,r1 #lllll
-	ldi r1,r1,r1#nullr1,r1 #lllll
-l:	ldi -345, r1, r1
-	live %2
-	zjmp %23
-	fork %4
-	lfork %10
+ldi r1,r1,r1
+ldi r1,r1,r1
+ldi -345, r1, r1
+live %2
+zjmp %23
+fork %4
+lfork %10
