4d29cd189d13f2b7cc16d7c11e696f9a9679324d
Dorian BAFFIER <dbaffier@e1r2p1.42.fr>
2019-10-24 20:09:21 +0200

    asm fix

--- asm_dir/conversion/way_to_corewar.c
+++ asm_dir/conversion/way_to_corewar.c
@@ -6,32 +6,35 @@
 /*   By: mmonier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 22:36:59 by mmonier           #+#    #+#             */
-/*   Updated: 2019/10/21 21:48:14 by dbaffier         ###   ########.fr       */
+/*   Updated: 2019/10/24 20:09:15 by dbaffier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cw_hexdump.h"
 
-int		way_to_corewar(t_data *data, t_aolist *ao)
+static void		split_way(t_data *data, t_aolist *a)
+{
+	if (!ft_strcmp(a->tok->lab, NAME_CMD_STRING))
+	{
+		write_name(data, a->tok->next->lab);
+		write_comment(data, a->next->tok->next->lab);
+	}
+	else
+	{
+		write_name(data, a->next->tok->next->lab);
+		write_comment(data, a->tok->next->lab);
+	}
+}
+
+int				way_to_corewar(t_data *data, t_aolist *ao)
 {
 	t_token *tok;
 
 	while (ao && ao->tok == NULL)
 		ao = ao->next;
 	write_magic(data);
-	if (!ft_strcmp(ao->tok->lab, NAME_CMD_STRING))
-	{
-		write_name(data, ao->tok->next->lab);
-		ao = ao->next;
-		write_comment(data, ao->tok->next->lab);
-		ao = ao->next;
-	}
-	else
-	{
-		write_name(data, ao->next->tok->next->lab);
-		write_comment(data, ao->tok->next->lab);
-		ao = ao->next->next;
-	}
+	split_way(data, ao);
+	ao = ao->next->next;
 	while (ao)
 	{
 		tok = ao->tok;
